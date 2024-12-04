4a387068a9a887a9851d8c15709c16c87a1a2d62
alcercu <333aleix333@gmail.com>
2024-12-04 11:55:45 +0100

    fix: vote is undefined if the juror didn't vote

--- src/app/[locale]/case/[disputeId]/components/Votes.tsx
+++ src/app/[locale]/case/[disputeId]/components/Votes.tsx
@@ -39,10 +39,10 @@ const Votes: React.FC<IVotes> = async ({ disputeId }) => {
 
   const jurors = voteData.rounds[0].drawnJurors.reduce(
     (acc: IJurorData, draw) => {
-      const juror = draw.vote.juror.id;
+      const juror = draw.vote?.juror.id;
       if (acc[juror]) {
         acc[juror].weight = acc[juror].weight + 1;
-      } else {
+      } else if (typeof juror !== "undefined") {
         acc[juror] = {
           shortAddress: shortenAddress(juror),
           weight: 1,
